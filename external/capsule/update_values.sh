46cfeeea7fc9d616ecaad98a6d80e24de55580bb
Hang Xu <hang.xu@zenseact.com>
2024-12-30 22:55:17 +0800

    modify scripts

---
+++ external/capsule/update_values.sh
@@ -0,0 +1,28 @@
+#！/bin/bash
+
+# define chart env
+export helm_chart_version='0.7.1' # need to check the actual chart version format
+export helm_repo_name='projectcapsule'
+export helm_repo_host='https://projectcapsule.github.io/charts'
+export helm_chart_name='capsule' # need to check the actual file name
+export helm_ns='capsule-system'
+export helm_release_name='capsule'
+
+# define harbor env
+export harbor_host='harbor.hanxux.local'
+export harbor_project='platform-external'
+export harbor_username='admin'
+export harbor_passwd='Harbor12345'
+
+# define working directory
+export work_dir='/home/s0001969/Cloud/helm-charts'
+
+#print command before executing
+set -x
+
+# diff upgrade
+helm registry login https://$harbor_host --insecure --username $harbor_username --password $harbor_passwd
+helm diff upgrade -i $capsule_release_name -n $helm_ns oci://$harbor_host/$harbor_project/$helm_chart_name --version $helm_chart_version -f ./values.yaml --insecure-skip-tls-verify
+
+# upgrade helm chart from harbor
+helm upgrade -i $capsule_release_name -n $helm_ns oci://$harbor_host/$harbor_project/$helm_chart_name --version $helm_chart_version -f ./values.yaml --insecure-skip-tls-verify
--- external/capsule/upgrade_helm_chart.sh
+++ external/capsule/upgrade_helm_chart.sh
@@ -1,14 +1,16 @@
 #！/bin/bash
 
 # define chart env
-export helm_chart_version_new='0.6.2' # need to check the actual chart version format
+export helm_chart_version_new='0.7.2' # need to check the actual chart version format
 export helm_repo_name='projectcapsule'
 export helm_repo_url='https://projectcapsule.github.io/charts'
 export helm_chart_name='capsule' # need to check the actual file name
 
 # define harbor env
-export harbor_url='https://harbor.hanxux.local'
+export harbor_host='harbor.hanxux.local'
 export harbor_project='platform-external'
+export harbor_username='admin'
+export harbor_passwd='Harbor12345'
 
 # define working directory
 export work_dir='/home/s0001969/Cloud/helm-charts'
@@ -19,11 +21,14 @@ set -x
 # pull helm chart
 helm repo add --force-update $helm_repo_name $helm_repo_url
 helm repo update $helm_repo_name
+echo
+
 cd $work_dir
 helm pull $helm_repo_name/$helm_chart_name --version $helm_chart_version_new
+echo
 
 # push to harbor
-helm registry login $harbor_url --insecure
-helm push $helm_chart_name-$helm_chart_version_new.tgz oci://$harbor_url/$harbor_project/ --insecure-skip-tls-verify
-
+helm registry login $harbor_host --insecure --username $harbor_username --password $harbor_passwd
+helm push $helm_chart_name-$helm_chart_version_new.tgz oci://$harbor_host/$harbor_project/ --insecure-skip-tls-verify
+echo
 
